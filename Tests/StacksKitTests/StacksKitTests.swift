7af37f121652c849c975b20445d691b54e38d132
Thomas Rademaker <thomas@sparrowtek.com>
2023-01-30 16:48:53 -0500

    update to latest swift and support latest iOS only (for now)

--- Tests/StacksKitTests/StacksKitTests.swift
+++ Tests/StacksKitTests/StacksKitTests.swift
@@ -2,10 +2,4 @@ import XCTest
 @testable import StacksKit
 
 final class StacksKitTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(StacksKit().text, "Hello, World!")
-    }
 }
